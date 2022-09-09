277a311cc0f30305fd4028b900122cb442061db8
Braian <braianplaku@gmail.com>
2022-09-10 00:50:08 +0200

    Fixing merge problems and adding images

--- webservice/run.py
+++ webservice/run.py
@@ -6,34 +6,30 @@ class Server:
         self.to_server = to_server
         self.from_server = from_server
 
-@app.route("/")
-def run_homepage():
-    database = db.get()
-    sql_data = database.get_sample()
-    sql_new = database.get_new_count()
-    sql_new = sql_new[0]
-    print(sql_new[0])
-    return render_template('index.html', data=sql_data, new=sql_new["count"])
-
-@app.route("/new")i
-def run_new():
-    database = db.get()
-    sql_data = database.get_sample()
-    sql_new = database.get_new_count()
-    sql_new = sql_new[0]
-    print(sql_new[0])
-    return render_template('new.html', data=sql_data, new=sql_new["count"])
-
         self.app = Flask(__name__, static_url_path="", static_folder="static")
         
         # register routes
         self.app.add_url_rule("/", view_func=self.homepage)
+        self.app.add_url_rule("/new", view_func=self.homepage)
 
 
     def homepage(self):
         database = db.get()
         sql_data = database.get_sample()
-        return render_template('index.html', data=sql_data)
+        sql_new = database.get_new_count()
+        sql_new = sql_new[0]
+        print(sql_new[0])
+        return render_template('index.html', data=sql_data, new=sql_new["count"])
+
+    def newpage(self):
+        database = db.get()
+        sql_data = database.get_sample()
+        sql_new = database.get_new_count()
+        sql_new = sql_new[0]
+        print(sql_new[0])
+        return render_template('new.html', data=sql_data, new=sql_new["count"])
+
+
 
     def run(self, port=5000, debug=True):
         self.app.run(host="0.0.0.0", port=port, debug=debug)
