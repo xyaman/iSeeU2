922da38bbdcdfc669eade8e14568dfb48459fcec
Braian <braianplaku@gmail.com>
2022-09-09 23:09:01 +0200

    added comments to code
    return values of the functions

--- webservice/db.py
+++ webservice/db.py
@@ -1,10 +1,12 @@
 import sqlite3 as sql
 
 class DB:
+    # establishes connection to database
     def __init__(self):
         self.con = sql.connect("RecDB2")
         self.con.row_factory = sql.Row
      
+    # returns desired query from desired table
     def get_data(self, table_name, query="*"):
         self.cur = self.con.cursor()
         table_cur = self.cur.execute(f"SELECT {query} FROM {table_name};")
@@ -12,15 +14,13 @@ class DB:
         data = table_cur.fetchall()
 
         return data
-
+    
+    # returns time, relative path to photo, name of person
     def get_sample(self):
         self.cur = self.con.cursor()
         table_cur = self.cur.execute(f"SELECT f.time, f.path, p.fname FROM Face f JOIN Person p ON f.person_id=p.id;")
 
         data = table_cur.fetchall()
-        for a in data:
-            print("------------")
-            print(a)
 
         return data
 
